d5edb3730257aa3eaf89cb0193be741817735997
priyanshoe <priyanshurahate@gmail.com>
2025-05-01 21:46:10 +0530

    Scroll bug fixed; gsap added to move element

--- script.js
+++ script.js
@@ -1,59 +1,81 @@
-const images = ["./images/png1.gif",'./images/png2.gif','./images/png3.gif'];
+const images = ["./images/png1.gif", './images/png2.gif', './images/png3.gif'];
 const container = document.querySelector('.mediaCircle img');
 const heading = document.querySelectorAll('.heading h1');
 const mediaCircle = document.querySelector('.mediaCircle');
+const head = document.querySelector('.heading')
 
 
-heading.forEach((elem,id)=>{
-    elem.addEventListener('mousemove',(dets)=>{
-        mediaCircle.style.left=dets.x+'px';
-        mediaCircle.style.top=dets.y+'px';
-        container.src = images[id];
+heading.forEach((elem, id) => {
+    elem.addEventListener('mousemove', (dets) => {
         
+        gsap.to(mediaCircle, {
+            x: dets.clientX + 'px',
+            y: dets.clientY  + 'px',
+            duration:.5
+
+        });
+        container.src = images[id];
+
     })
 })
 
 
 
-function leaveEnter(){
-    const head = document.querySelector('.heading');
-    head.addEventListener('mouseenter',()=>{
-        mediaCircle.style.scale=1
-    })
-    head.addEventListener('mouseleave',()=>{
-        mediaCircle.style.scale=0
-    })
+function leaveEnter() {
+    head.addEventListener('mouseenter', () => {
+
+        gsap.to(mediaCircle,
+            {
+                scale: 1,
+                duration: 0.3
+            });
+    });
+
+    head.addEventListener('mouseleave', () => {
+        gsap.to(mediaCircle,
+            {
+                scale: 0,
+                duration: 0.3
+            });
+    });
 }
 leaveEnter();
 
 
-function costumCursor(){
+function costumCursor() {
     const cursor = document.querySelector('.cursor');
-    window.addEventListener('mousemove',(dets)=>{
-        cursor.style.left = dets.clientX +'px';
-        cursor.style.top = dets.clientY +'px';
+    
+    window.addEventListener('mousemove', (dets) => {
+
+        gsap.to(cursor, {
+            x: dets.clientX + 'px',
+            y: dets.clientY + 'px',
+            duration: .3
+
+        })
     })
 }
 costumCursor();
 
 
-gsap.to('.imageSlider',{
-    scrollTrigger:{
-        trigger:'.imageContainer3',
-        start:"top 10%",
-        end:'top -90%',
-        scrub:4,
-        pin:true
+gsap.to('.imageSlider', {
+    scrollTrigger: {
+        trigger: '.imageContainer3',
+        start: "top 14%",
+        end: 'top -90%',
+        scrub: 4,
+        pin: true,
+        // markers:true,
+
     },
-    y:"-303%"
+    y: "-303%"
 })
 
-gsap.to('.page3',{
-    scrollTrigger:{
-        trigger:'.page3',
-        start:"top top",
-        end:'top -100%',
-        pin:true,
-        // markers:true,
+gsap.to('.page3', {
+    scrollTrigger: {
+        trigger: '.page3',
+        start: "top top",
+        end: 'top -100%',
+        pin: true,
     }
 })
